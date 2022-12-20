cbe7ec171655292651e8d9db084a1265abc76f26
Taross <say_chan.gaki0507@icloud.com>
2022-12-20 16:15:07 +0900

    勝者の判定まで

--- src/index.js
+++ src/index.js
@@ -1,67 +1,110 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
+const Square = (props) => {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
         </button>
-      );
-    }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
+    );
+};
+
+const Board = (props) => {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+
+    const handleClick = (i) => {
+        const newSquares = squares.slice();
+        if (calculateWinner(newSquares) || newSquares[i]) {
+            return;
+        }
+        newSquares[i] = xIsNext ? "X" : "O";
+        setSquares(newSquares);
+        setXIsNext(!xIsNext);
+    };
+
+    const renderSquare = (i) => (
+        <Square
+            value={squares[i]}
+            onClick={() => {
+                handleClick(i);
+            }}
+        />
+    );
+
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (
+                squares[a] &&
+                squares[a] === squares[b] &&
+                squares[a] === squares[c]
+            ) {
+                return squares[a];
+            }
+        }
+        return null;
+    };
+
+    const status = () => {
+        const winner = calculateWinner(squares);
+        if (winner) {
+            return "Winner: " + winner;
+        } else if (squares.reduce((x, y) => x && y)) {
+            return "Draw";
+        } else {
+            return `Next player: ${xIsNext ? "X" : "O"}`;
+        }
+    };
+
+    return (
         <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+            <div className="status">{status()}</div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
         </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
+    );
+};
+
+const Game = (props) => {
+    return (
         <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+            </div>
         </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
-  
-  
+    );
+};
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
